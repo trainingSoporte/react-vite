cca659d16fb76de2ca6962c5f8b55beb6dc0acfc
Adrian Ojeda <AOJEDA@claro.com.ar>
2024-07-19 17:53:06 -0300

    migra

---
+++ app-monitor/src/common/constants/index.ts
@@ -0,0 +1,15 @@
+export const ROUTE_ECOMMERCE =
+{
+    path: "/ecommerce",
+    icon: "fa-solid fa-wrench",
+    title: "Ecommerce",
+    description: "Ecommerce controls",
+};
+
+export const ROUTE_OTHERS =
+{
+    path: "/other-menu-options",
+    icon: "fa-solid fa-wrench",
+    title: "title other",
+    description: "description other",
+};
